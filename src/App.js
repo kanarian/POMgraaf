708deba903a30f9042955c3d6d1043f2804d88da
kanarian <arian.joyandeh@hotmail.com>
2022-07-14 21:10:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import ForceGraph3D from 'react-force-graph-3d';
+import { useState, useEffect, useRef } from 'react';
+import Select from 'react-select';
+import links from './data/links.json';
+import nodes from './data/users.json';
+
 
 function App() {
+  const [graphData,setGraphData] = useState({nodes: nodes, links: links})
+  const [selectedUser, setSelectedUser] = useState({})
+
+
+  // it should not be needed to initialize this in the useEffect hook, but if I dont do it, it doesnt work
+  useEffect(() => {
+    setGraphData({nodes: nodes, links: links})
+  }, [])
+
+
+  // when the selected user changes, the camera should pan to that position.
+  useEffect( () => {
+    if (selectedUser.value !== undefined){
+      console.log(selectedUser)
+      var selectedUserNode = graphData.nodes.find(x => x.id === selectedUser.value);
+      console.log(selectedUserNode)
+      const distance = 70;
+      const distRatio = 1 + distance/Math.hypot(selectedUserNode.x, selectedUserNode.y, selectedUserNode.z);
+      fgRef.current.cameraPosition(
+        { x: selectedUserNode.x * distRatio, y: selectedUserNode.y * distRatio, z: selectedUserNode.z * distRatio }, // new position
+        selectedUserNode, // lookAt ({ x, y, z })
+        3000  // ms transition duration
+      );
+    }}, [selectedUser])
+  
+
+  const dropdownOptions = nodes.map(entry => {return {value: entry.id, label: entry.name[0]}})
+
+  const fgRef = useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+        <div className='header'>
+          <div>
+          <h1>POM telegram groep interactie graaf</h1>
+          <p>Users zijn gelinkt wanneer er minstens een reply interactie tussen hen is geweest. Hoe dikker de lijn, hoe meer interacties.</p>
+          </div>
+          <div className='username-finder'>
+            <p><strong>Zoek een user:</strong></p>
+            <Select 
+              onChange={e=>setSelectedUser(e)}
+              options={dropdownOptions}
+            />
+          </div>     
+        </div>
+
+        <ForceGraph3D
+        ref = {fgRef}
+        graphData = {graphData}
+        linkSource='userOne'
+        linkTarget='userTwo'
+        nodeOpacity={0.9}
+        backgroundColor="#4664bd"
+        nodeColor = {x => x.id === selectedUser.value ? 'yellow' : '#df7a64'}
+        linkColor = {x => 'white'}
+        linkWidth = {x => x.value/10}
+        />
+      </div>
   );
 }
 
